138e25066171bcb779b7eb1b1dd7a4ad7136592d
sw092180 <stephen.kolar@hpe.com>
2017-06-29 10:34:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
--Dsonar.log.console=true \
--Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
--Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
--Dsonar.jdbc.url="$SONARQUBE_JDBC_URL"
+echo "HELLO WORLD!"
